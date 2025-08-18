9513194bd5568df42e86e73fe885796e68b07c09
FmcStore <guardianmarx@gmail.com>
2025-08-18 20:25:05 +0700

    Update sendTelegram.js

--- utils/sendTelegram.js
+++ utils/sendTelegram.js
@@ -1,13 +1,19 @@
+// utils/sendTelegram.js
 import axios from 'axios';
 
 const TOKEN = "7690886756:AAFu6e3Xerck9rNSCufwCerp-ooaX6rLqbc";
 const CHAT_ID = "8193547847";
+const URL = `https://api.telegram.org/bot${TOKEN}/sendMessage`; // perbaiki spasi berlebih
 
 export async function sendTelegram(message) {
-  const url = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
-
-  await axios.post(url, {
-    chat_id: CHAT_ID,
-    text: message,
-  });
+  try {
+    await axios.post(URL, {
+      chat_id: CHAT_ID,
+      text: message,
+      parse_mode: 'HTML', // agar bisa pakai bold, dll
+    });
+  } catch (err) {
+    console.error('Gagal kirim ke Telegram:', err.response?.data || err.message);
+    // Tetap jalan meski gagal kirim notif
+  }
 }
