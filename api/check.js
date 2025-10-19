e43c86e12bcae3e467c7839acced305805470b6a
FmcStore <guardianmarx@gmail.com>
2025-10-19 19:37:36 +0700

    Change target URL in check.js

--- api/check.js
+++ api/check.js
@@ -4,7 +4,7 @@ import { getStatus, setStatus } from '../utils/statusStore.js';
 import axios from 'axios';
 
 export default async function handler(req, res) {
-  const targetURL = "https://api.siputzx.my.id/api/currency/rates";
+  const targetURL = "https://gdnstore.my.id";
   const currentStatus = getStatus();
 
   const now = new Date();
