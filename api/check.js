20ddc924042a2caf228adecab036cdf223c14ef0
FmcStore <guardianmarx@gmail.com>
2025-12-28 16:31:50 +0700

    Update target URL in check.js

--- api/check.js
+++ api/check.js
@@ -4,7 +4,7 @@ import { getStatus, setStatus } from '../utils/statusStore.js';
 import axios from 'axios';
 
 export default async function handler(req, res) {
-  const targetURL = "https://gdnstore.my.id/api/track.js";
+  const targetURL = "https://fmcstore.web.id";
   const currentStatus = getStatus();
 
   const now = new Date();
